09f59b75e1f04ad6bafaa52f11799fd8b899c8e3
Jacob Jerrell <jacob@jjerrell.dev>
2023-06-09 22:50:40 -0500

    Create MenuListItem composable

---
+++ app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/composables/components/MenuListItem.kt
@@ -0,0 +1,91 @@
+package app.jjerrell.meta.course.sample.littlelemon.composables.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.jjerrell.meta.course.sample.littlelemon.model.MenuItem
+import app.jjerrell.meta.course.sample.littlelemon.ui.theme.LittleLemonTheme
+
+@Composable
+fun MenuListItem(
+    modifier: Modifier = Modifier,
+    isEven: Boolean,
+    item: MenuItem
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth().then(
+                if (isEven)
+                    Modifier.background(MaterialTheme.colorScheme.secondary)
+                else
+                    Modifier.background(MaterialTheme.colorScheme.primary)
+            )
+            .padding(12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.weight(2F)
+        ) {
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.titleLarge,
+                color = with(MaterialTheme.colorScheme) {
+                    if (isEven) Color.Unspecified
+                    else onSecondary
+                }
+            )
+            Text(
+                text = item.description,
+                style = MaterialTheme.typography.bodyLarge,
+                color = with(MaterialTheme.colorScheme) {
+                    if (isEven) Color.Unspecified
+                    else onSecondary
+                }
+            )
+            Text(
+                text = "$" + "${item.price}",
+                style = MaterialTheme.typography.headlineLarge,
+                color = with(MaterialTheme.colorScheme) {
+                    if (isEven) Color.Unspecified
+                    else onSecondary
+                }
+            )
+        }
+        Image(
+            painterResource(id = item.image),
+            modifier = Modifier.weight(1F),
+            contentDescription = "An image of the ${item.title} dish"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MenuListItem_Preview() {
+    LittleLemonTheme {
+        LazyColumn {
+            itemsIndexed(MenuItem.defaultMenu) {index, item ->
+                MenuListItem(
+                    item = item,
+                    isEven = index % 2 == 0
+                )
+            }
+        }
+    }
+}
