28dcff54ed11478893fbfed60d3f1e1d8646d2fe
Jacob Jerrell <jacob@jjerrell.dev>
2023-06-09 22:49:37 -0500

    Cleanup, add top level opt-ins, import profile image

--- app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/MainActivity.kt
+++ app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/MainActivity.kt
@@ -4,24 +4,28 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import app.jjerrell.meta.course.sample.littlelemon.navigation.AppFlow
 import app.jjerrell.meta.course.sample.littlelemon.navigation.enrollmentNavigation
 import app.jjerrell.meta.course.sample.littlelemon.navigation.mainNavigation
 import app.jjerrell.meta.course.sample.littlelemon.ui.theme.LittleLemonTheme
 
 const val DATA_STORE_NAME = "LittleLemonMenu"
-val KEY_FIRST_NAME = stringPreferencesKey("firstName")
-val KEY_LAST_NAME = stringPreferencesKey("lastName")
-val KEY_EMAIL = stringPreferencesKey("email")
-
 val Context.dataStore by preferencesDataStore(DATA_STORE_NAME)
 
-@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalComposeUiApi::class,
+    ExperimentalFoundationApi::class,
+    ExperimentalLayoutApi::class,
+    ExperimentalMaterial3Api::class
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,18 +35,12 @@ class MainActivity : ComponentActivity() {
                 NavHost(
                     navController = navController,
                     startDestination = AppFlow.LOGIN.route,
+                    modifier = Modifier,
                     builder = {
                         enrollmentNavigation(controller = navController)
                         mainNavigation(controller = navController)
                     }
                 )
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//
-//                }
             }
         }
     }
