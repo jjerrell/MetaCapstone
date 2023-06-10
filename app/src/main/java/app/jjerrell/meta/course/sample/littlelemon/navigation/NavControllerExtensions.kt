fa8b6f2f765eb4ad4f7ec577b95c25a4602a968d
Jacob Jerrell <jacob@jjerrell.dev>
2023-06-09 22:37:30 -0500

    Update Navigation and add string resources

--- app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/navigation/NavControllerExtensions.kt
+++ app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/navigation/NavControllerExtensions.kt
@@ -4,8 +4,6 @@ import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.Navigator
-import app.jjerrell.meta.course.sample.littlelemon.AppFlow
-import app.jjerrell.meta.course.sample.littlelemon.AppFlowRoute
 
 /**
  * Type safe navigation control for in-flow calls
