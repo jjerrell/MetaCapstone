4ad7942cb37a2eaf33610a30024c66952952d631
Jacob Jerrell <jacob@jjerrell.dev>
2023-06-10 09:30:32 -0500

    Give more control to parents of the LLHero composable

--- app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/composables/components/LLHero.kt
+++ app/src/main/java/app/jjerrell/meta/course/sample/littlelemon/composables/components/LLHero.kt
@@ -1,22 +1,30 @@
 package app.jjerrell.meta.course.sample.littlelemon.composables.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
 fun LLHero(
     modifier: Modifier = Modifier,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxHeight(0.2f)
+            .background(MaterialTheme.colorScheme.primary),
+        horizontalAlignment = horizontalAlignment,
+        verticalArrangement = verticalArrangement
     ) {
         content()
     }
